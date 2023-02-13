e9c57aacdcf70563052cba78602f82821266fb48
oppressor117 <oppressor117@gmail.com>
2023-02-13 14:31:36 +0300

    comments added

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] array1 = new string[6] {"hello", "world", "fin", "task", "333", "excellent"};
 string[] array2 = new string[array1.Length];
 
-void NewArray(string[] array1, string[] array2)
+void NewArray(string[] array1, string[] array2) // метод формирования массива из строк, длина которых меньше либо равна 3 символа
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
@@ -14,7 +14,7 @@ void NewArray(string[] array1, string[] array2)
     }
 }
 
-void PrintArr(string[] arr)
+void PrintArr(string[] arr) // метод оформление массива
 {
     Console.Write("[");
     for(int i = 0; i < arr.Length-1; i++)
@@ -23,6 +23,7 @@ void PrintArr(string[] arr)
     }
     Console.WriteLine(arr[arr.Length-1]+"]");
 }
+
 PrintArr(array1);
 NewArray(array1, array2);
 PrintArr(array2);
